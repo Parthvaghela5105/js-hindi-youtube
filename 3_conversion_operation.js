1259ac8b802319d52bafb37fcf364bc298075080
Parthvaghela5105 <133079004+Parthvaghela5105@users.noreply.github.com>
2024-05-11 17:26:05 +0000

    operation

--- 3_conversion_operation.js
+++ 3_conversion_operation.js
@@ -23,4 +23,22 @@ let isLoggedIn = 0;
 
 let booleanIsLoggedIn = String(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// ************************************Operation********************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/3)
+console.log(2**3)
+console.log(2%3)
+
+let str1 = "Hello"
+let str2 = "Parth"
+let str = str1 + str2
+console.log(str)
