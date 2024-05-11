d72108028f750c751b1269f9980445ab1095bc5f
Parthvaghela5105 <133079004+Parthvaghela5105@users.noreply.github.com>
2024-05-11 17:10:24 +0000

    conversion of datatypes

---
+++ 3_conversion_operation.js
@@ -0,0 +1,26 @@
+// let score = "33abc";
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+/*
+    conver into number
+    "33" => 33
+    "33abc" => NaN
+    true => 1
+    false => 0
+    undfiend => NaN
+    null => 0
+*/
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = String(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
